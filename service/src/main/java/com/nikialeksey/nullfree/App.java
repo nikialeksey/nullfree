fbeb8ccd2e08bd7c064ac142b6cc6f9f5b11447d
Alexey Nikitin <nikialeksey@gmail.com>
2019-04-28 13:38:56 +0700

    Repo persistence

--- service/src/main/java/com/nikialeksey/nullfree/App.java
+++ service/src/main/java/com/nikialeksey/nullfree/App.java
@@ -1,11 +1,22 @@
 package com.nikialeksey.nullfree;
 
+import com.nikialeksey.nullfree.db.MigrationsDb;
+import com.nikialeksey.nullfree.db.SimpleMigrations;
+import com.nikialeksey.nullfree.db.SqliteDb;
+import com.nikialeksey.nullfree.migrations.Migration0;
+import com.nikialeksey.nullfree.repo.DbRepos;
+import com.nikialeksey.nullfree.repo.RepoInfo;
 import org.takes.Request;
 import org.takes.Response;
 import org.takes.Take;
+import org.takes.facets.fallback.FbChain;
+import org.takes.facets.fallback.FbStatus;
+import org.takes.facets.fallback.TkFallback;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
+import org.takes.rs.RsText;
 
+import java.io.File;
 import java.io.IOException;
 
 public class App implements Take {
@@ -14,16 +25,32 @@ public class App implements Take {
 
     public App() {
         this(
-            new TkFork(
-                new FkRegex("/nullfree(/)?", "Nullfree"),
-                new FkRegex(
-                    "/nullfree/.+",
-                    new TkFork(
-                        new FkRegex(
-                            "/nullfree/(?<user>[^/]+)/(?<repo>[^/]+)",
-                            new RepoInfo()
+            new TkFallback(
+                new TkFork(
+                    new FkRegex("/nullfree(/)?", "Nullfree"),
+                    new FkRegex(
+                        "/nullfree/.+",
+                        new TkFork(
+                            new FkRegex(
+                                "/nullfree/(?<user>[^/]+)/(?<repo>[^/]+)",
+                                new RepoInfo(
+                                    new DbRepos(
+                                        new MigrationsDb(
+                                            new SqliteDb(new File("./iwfy.db")),
+                                            new SimpleMigrations(
+                                                new Migration0()
+                                            ),
+                                            1
+                                        )
+                                    )
+                                )
+                            )
                         )
                     )
+                ),
+                new FbChain(
+                    new FbStatus(404, new RsText("sorry, page is absent")),
+                    new FbStatus(405, new RsText("this method is not allowed here"))
                 )
             )
         );
