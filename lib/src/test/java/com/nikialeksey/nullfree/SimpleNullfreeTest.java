69e7f6f1bcac5123f173be730cd8268d93219fa7
Alexey Nikitin <nikialeksey@gmail.com>
2019-06-16 00:33:57 +0700

    #17 Extract shields.io logic to iwillfailyou project

--- lib/src/test/java/com/nikialeksey/nullfree/SimpleNullfreeTest.java
+++ lib/src/test/java/com/nikialeksey/nullfree/SimpleNullfreeTest.java
@@ -1,29 +1,14 @@
 package com.nikialeksey.nullfree;
 
-import com.nikialeksey.nullfree.badge.ShieldsIoBadge;
+import com.nikialeksey.nullfree.badge.SimpleBadge;
 import com.nikialeksey.nullfree.sources.SimpleSources;
 import com.nikialeksey.nullfree.sources.java.JavaSourceFile;
-import com.nikialeksey.nullfree.sources.java.JavaSourceFileFactory;
-import org.hamcrest.core.IsEqual;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
-import java.net.URL;
 import java.util.Collections;
 
 public class SimpleNullfreeTest {
-    @Test
-    public void currentProjectBadgeCheck() throws Exception {
-        URL url = new SimpleNullfree(
-            new SimpleSources(
-                new File("./src/"),
-                new JavaSourceFileFactory()
-            ),
-            ShieldsIoBadge::new
-        ).badge().asUrl();
-        Assert.assertThat(url.toString(), IsEqual.equalTo("https://img.shields.io/badge/nullfree-approved-green.svg"));
-    }
 
     @Test
     public void failIfRedWhenRed() throws Exception {
@@ -51,7 +36,7 @@ public class SimpleNullfreeTest {
                         )
                     )
                 ),
-                ShieldsIoBadge::new
+                SimpleBadge::new
             ).badge().failIfRed();
             Assert.fail();
         } catch (NullfreeException e) {
@@ -74,7 +59,7 @@ public class SimpleNullfreeTest {
                         )
                     )
                 ),
-                ShieldsIoBadge::new
+                SimpleBadge::new
             ).badge().failIfRed();
             // green
         } catch (NullfreeException e) {
